c6ac20b17fcb2632ed8f1808f58388c7cf82735c
Lora <143578889+Lora207@users.noreply.github.com>
2023-11-02 03:36:27 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    }
+
+    const data = [];
+
+function generateRandomData() {
+}
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+main();
+function main() {
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+}
+    });
+    return data;
